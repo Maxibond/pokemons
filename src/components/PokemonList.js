741119e21bb25f975a79726b3949c175ba254fad
KapitanBanan <shukov1995@gmail.com>
2019-12-02 21:36:03 +0500

    #1 Pagination in PokemonList (#1)
    * Added Next, Previous buttons in PokemonList
    
    * Use localStorage for PokemonList
    
    * removed Pokemon List import from Pokemon
    
    * Fixes PR comments

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -3,31 +3,67 @@ import { Link } from "react-router-dom";
 
 
 class PokemonList extends Component {
+
+    limit = 20;
+    pageKey = "pokemons.page";
     
     constructor(props) {
         super(props);
 
+        const page = localStorage.getItem(this.pageKey) || 1;
+        
+        localStorage.setItem(this.pageKey, page);
+        
+        const offset = (page-1)*this.limit;
+
         this.state = { 
             pokemons: [],
-            next: "",
-            previous: "",
+            offset,
+            limit: this.limit,
+            count: 0,
         }
+
+        this.paginator = this.paginator.bind(this);
     }
 
     async componentDidMount() {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        this.fetchPokemons();
+    }
+    
+    async fetchPokemons() {
+        const {offset, limit} = this.state;
+        const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+        const response = await fetch(url);
         const result = await response.json();
         this.setState({
-            pokemons: result.results
+            pokemons: result.results,
+            count: result.count,
         });
+    }
+    
+    paginator(isNext) {
+        const {offset, limit} = this.state;
+
+        const newOffset = isNext ? offset+limit: offset-limit;
+
+        this.setState({
+            offset: newOffset,
+        }, () => this.fetchPokemons());
+        localStorage.setItem(this.pageKey, newOffset/limit+1);
     }
 
     render() {
+        const {
+            offset,
+            limit,
+            count,
+            pokemons,
+        } = this.state;
         return (
             <div>
                 <h1>Pokemons</h1> 
                 {
-                    this.state.pokemons.map(p => {
+                    pokemons.map(p => {
                         const pokemonID = getPokemonID(p.url);
                         return (
                             <div key={p.name}>
@@ -36,6 +72,16 @@ class PokemonList extends Component {
                         );
                     })
                 }
+                <button disabled={offset<=0} 
+                        onClick={() => this.paginator(false)}
+                        >
+                            Previous
+                </button>
+                <button disabled={offset+limit>count} 
+                        onClick={ () => this.paginator(true)}
+                        >
+                            Next
+                </button>
             </div>
         )
     }
